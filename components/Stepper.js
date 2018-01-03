a73bea19ed2f17516a5435dbecf36bb6c01bdf1c
Lukasz Janyst <lukasz@jany.st>
2018-01-03 22:26:41 +0100

    Add stubs for Slider and Stepper components

---
+++ components/Stepper.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Slider(props) {
+  return (
+    <View>
+      <Text>Stepper</Text>
+    </View>
+  );
+}
