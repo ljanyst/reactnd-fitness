0943d90c87bf25543188f0af577422e0688982bf
Lukasz Janyst <lukasz@jany.st>
2018-01-04 07:43:06 +0100

    Implement Stepper

--- components/Stepper.js
+++ components/Stepper.js
@@ -1,11 +1,24 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
 
 export default function Slider(props) {
+  const { max, unit, step, value, onIncrement, onDecrement } = props;
   return (
     <View>
-      <Text>Stepper</Text>
+      <View>
+        <TouchableOpacity onPress={onDecrement}>
+          <FontAwesome name='minus' size={30} color='black'/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={onIncrement}>
+          <FontAwesome name='plus' size={30} color='black'/>
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
   );
 }
