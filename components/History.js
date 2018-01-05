11cee6ad63dea76e772ebfaacab3b0f82524537a
Lukasz Janyst <lukasz@jany.st>
2018-01-05 22:59:29 +0100

    Add no data entries with styles to History

--- components/History.js
+++ components/History.js
@@ -1,12 +1,51 @@
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Text, StyleSheet, Platform, TouchableOpacity
+} from 'react-native';
 import { connect } from 'react-redux';
 import UdaciFitnessCalendar from 'udacifitness-calendar';
 
 import { receiveEntries, addEntry } from '../actions';
 import { timeToString, getDailyReminderValue } from '../utils/helpers';
 import { fetchCalendarResults } from '../utils/api';
+import { white } from '../utils/colors';
+
+import DateHeader from './DateHeader';
+
+//------------------------------------------------------------------------------
+// Styles
+//------------------------------------------------------------------------------
+const styles = StyleSheet.create({
+  //----------------------------------------------------------------------------
+  // Item
+  //----------------------------------------------------------------------------
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0,0,0,0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    }
+  },
+
+  //----------------------------------------------------------------------------
+  // No data text
+  //----------------------------------------------------------------------------
+  noDataText: {
+    fontSize: 20,
+    paddingTop: 20,
+    paddingBottom: 20
+  }
+});
 
 //------------------------------------------------------------------------------
 // History
@@ -30,10 +69,15 @@ class History extends Component {
   // Render item
   //----------------------------------------------------------------------------
   renderItem = ({today, ...metrics}, formattedDate, key) => (
-    <View>
+    <View style={styles.item}>
       {
         today
-          ? <Text>{JSON.stringify(today)}</Text>
+        ? <View>
+            <DateHeader date={formattedDate}/>
+            <Text style={styles.noDataText}>
+              {today}
+            </Text>
+          </View>
           : <Text>{JSON.stringify(metrics)}</Text>
       }
     </View>
@@ -44,8 +88,11 @@ class History extends Component {
   //----------------------------------------------------------------------------
   renderEmptyDate(formattedDate) {
     return (
-      <View>
-        <Text>No Data for this day</Text>
+      <View style={styles.item}>
+        <DateHeader date={formattedDate}/>
+        <Text style={styles.noDataText}>
+          You didn't log any data on this day.
+        </Text>
       </View>
     );
   }
