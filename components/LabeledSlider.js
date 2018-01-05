706e946ed129e82ad2c2752b7944587b4e96027b
Lukasz Janyst <lukasz@jany.st>
2018-01-05 20:38:06 +0100

    Add style to LabeledSlider

--- components/LabeledSlider.js
+++ components/LabeledSlider.js
@@ -1,21 +1,46 @@
 
 import React from 'react';
-import { View, Text, Slider } from 'react-native';
+import { View, Text, Slider, StyleSheet } from 'react-native';
+import { gray } from '../utils/colors';
 
+//------------------------------------------------------------------------------
+// The style
+//------------------------------------------------------------------------------
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center'
+  },
+
+  //----------------------------------------------------------------------------
+  // Metric counter
+  //----------------------------------------------------------------------------
+  metricCounter: {
+    width: 85,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+
+//------------------------------------------------------------------------------
+// The component
+//------------------------------------------------------------------------------
 export default function LabeledSlider(props) {
   const { step, value, max, onChange, unit } = props;
   return (
-    <View>
+    <View style={styles.row}>
       <Slider
+        style={{flex: 1}}
         step={step}
         value={value}
         maximumValue={max}
         minimumValue={0}
         onValueChange={onChange}
       />
-      <View>
-        <Text>{value}</Text>
-        <Text>{unit}</Text>
+      <View style={styles.metricCounter}>
+        <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+        <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
       </View>
     </View>
   );
