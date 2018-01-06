723f8623461bf1254a35716319c0b8d070cf3537
Lukasz Janyst <lukasz@jany.st>
2018-01-06 17:58:19 +0100

    Implement EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,15 +1,95 @@
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
 
+import { white } from '../utils/colors';
+import { addEntry } from '../actions';
+import { removeEntry } from '../utils/api';
+import { timeToString, getDailyReminderValue } from '../utils/helpers';
+
+import MetricCard from './MetricCard';
+import TextButton from './TextButton';
+
+//------------------------------------------------------------------------------
+// Styles
+//------------------------------------------------------------------------------
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15
+  }
+});
+
+//------------------------------------------------------------------------------
+// Component
+//------------------------------------------------------------------------------
 class EntryDetail extends Component {
+  //----------------------------------------------------------------------------
+  // Navigation options
+  //----------------------------------------------------------------------------
+  static navigationOptions = ({ navigation }) => {
+    const { entryId } = navigation.state.params;
+    const year = entryId.slice(0, 4);
+    const month = entryId.slice(5, 7);
+    const day = entryId.slice(8);
+    return {
+      title: `${day}/${month}/${year}`
+    };
+  };
+
+  //----------------------------------------------------------------------------
+  // Reset
+  //----------------------------------------------------------------------------
+  reset = () => {
+    const { remove, navigation, entryId } = this.props;
+    remove();
+    navigation.goBack();
+    removeEntry(entryId);
+  };
+
+  //----------------------------------------------------------------------------
+  // Should update
+  //----------------------------------------------------------------------------
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics && !nextProps.metrics.today;
+  }
+
+  //----------------------------------------------------------------------------
+  // Render the component
+  //----------------------------------------------------------------------------
   render() {
     return (
-      <View>
-        <Text>Entry details - {this.props.navigation.state.params.entryId}</Text>
+      <View style={styles.container}>
+        <MetricCard metrics={this.props.metrics} />
+        <TextButton onPress={this.reset} style={{margin: 20}}>
+          RESET
+        </TextButton>
+
       </View>
     );
   }
 }
 
-export default EntryDetail;
+//------------------------------------------------------------------------------
+// Redux connection
+//------------------------------------------------------------------------------
+function mapStateToProps(state, { navigation }) {
+  const { entryId } = navigation.state.params;
+  return {
+    entryId,
+    metrics: state[entryId]
+  };
+}
+
+function mapDispatchToProps(dispatch, {navigation}) {
+  const { entryId } = navigation.state.params;
+  return {
+    remove: () => dispatch(addEntry({
+      [entryId]: timeToString() === entryId ? getDailyReminderValue() : null
+    }))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EntryDetail);
